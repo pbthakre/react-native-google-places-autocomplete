27e9a8dd040237b54e86060fc7cde75c060cdc08
Priyanka <pbtspl@gmail.com>
2019-02-06 17:20:13 +0530

    icon position fix

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -48,7 +48,7 @@ const defaultStyles = {
   },
   poweredContainer: {
     justifyContent: 'flex-end',
-    alignItems: 'center',
+   alignItems: 'flex-end',
     backgroundColor: '#FFFFFF',
   },
   powered: {},
@@ -56,7 +56,7 @@ const defaultStyles = {
   row: {
     //height: 44,
    // height:60,
-    flexDirection: 'row',
+    //flexDirection: 'row',
   },
   separator: {
    // height: StyleSheet.hairlineWidth,
@@ -527,21 +527,24 @@ export default class GooglePlacesAutocomplete extends Component {
   }
 
   _renderRowData = (rowData) => {
+    let firstLine = this._renderDescription(rowData, 'first');
+    let secondLine = this._renderDescription(rowData, 'last');
      if (this.props.renderRow) {
       return this.props.renderRow(rowData);
     }
     return (
-      <View style={{alignSelf : 'stretch', flex:1}}>
-        <Text style={[ {alignContent : 'flex-start', fontSize : scale(16)},this.props.suppressDefaultStyles ? {} : defaultStyles.description, this.props.styles.description, rowData.isPredefinedPlace ? this.props.styles.predefinedPlacesDescription : {}]}
-         numberOfLines={2}
-        >
-          {this._renderDescription(rowData, 'first')}
-        </Text>
-        <Text style={[ {fontSize : scale(14)},this.props.suppressDefaultStyles ? {} : defaultStyles.description, this.props.styles.description, rowData.isPredefinedPlace ? this.props.styles.predefinedPlacesDescription : {}]}
-        numberOfLines={2} ellipsizeMode={'tail'}
+      <View style={{overflow:'hidden', alignItems:'center'}}>
+      <View style={{marginRight:10,}}>
+       {firstLine !== ''? <Text style={[ { fontSize : scale(16)},this.props.suppressDefaultStyles ? {} : defaultStyles.description, this.props.styles.description, rowData.isPredefinedPlace ? this.props.styles.predefinedPlacesDescription : {}]}
+         numberOfLines={2}>
+          {firstLine}
+        </Text>: null }
+    {secondLine !== '' ?<Text style={[ {fontSize : scale(14)},this.props.suppressDefaultStyles ? {} : defaultStyles.description, this.props.styles.description, rowData.isPredefinedPlace ? this.props.styles.predefinedPlacesDescription : {}]}
+        numberOfLines={4} ellipsizeMode={'tail'}
       >
-         {this._renderDescription(rowData, 'last')}
-  </Text>
+         {secondLine}
+  </Text>: null }
+  </View>
     </View>
     );
   }
@@ -576,14 +579,9 @@ export default class GooglePlacesAutocomplete extends Component {
 
   _renderRow = (rowData = {}, sectionID, rowID) => {
    return (
-      <ScrollView
-        style={{ flex: 1, flexDirection:'row', }}
-        scrollEnabled={this.props.isRowScrollable}
-        contentContainerStyle={{alignItems:'center' }}
-        keyboardShouldPersistTaps={this.props.keyboardShouldPersistTaps}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        showsVerticalScrollIndicator={false}>
+      <View
+        style={{ flex: 1, flexDirection:'row', alignItems:'center'}}
+        >
         <Image resizeMode='contain' style= {{ marginTop:10, marginRight:7, width:scale(22), height:verticalScale(36)}} source ={require('./Assets/Locationpin.png')}/>
         <TouchableHighlight
           // style={{ width: WINDOW.width}}
@@ -595,7 +593,7 @@ export default class GooglePlacesAutocomplete extends Component {
             {this._renderLoader(rowData)}
           </View>
         </TouchableHighlight>
-      </ScrollView>
+      </View>
     );
   }
 
